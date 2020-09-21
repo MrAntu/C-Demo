c42db0f0cbbf743417b6d09cefd7a7771c63309c
weiweili <weiweili@hk01.com>
2020-09-21 16:16:54 +0800

    fix: update

---
+++ C++学习/C++学习/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++学习
+//
+//  Created by weiwei.li on 2020/8/10.
+//  Copyright © 2020 dd01.leo. All rights reserved.
+//
+
+#include <iostream>
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    return 0;
+}
